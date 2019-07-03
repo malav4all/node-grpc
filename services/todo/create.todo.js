7a8228bd68afb4310af2495bf08764e55eee0243
akinmaurice <akinmaurice@gmail.com>
2019-07-03 12:04:22 +0100

    refactor server

--- services/todo/create.todo.js
+++ services/todo/create.todo.js
@@ -2,11 +2,8 @@ const db = require('../../db');
 
 
 function createTodo(call, callback) {
-    console.log('Creating todo');
     const todo = call.request;
-    let id = Math.floor(Math.random() * 1000000) + 1;
-    id = id.toString();
-    todo.id = id;
+    const { id } = todo;
     db.saveTodo(id, todo)
         .then(() => {
             callback(null, todo);
